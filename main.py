55343ddcb7003537020fa015094f2b5074e85ea8
isaiah <isaiahballah@gmail.com>
2019-01-08 21:26:46 -0500

    OpenCV cam in separate thread to improve fps

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import time
-
 import imutils
 import cv2
 from imutils.video import FPS
 from threading import Thread
-from queue import Queue
 
 
 class CameraStream:
-    def __init__(self, queueSize = 3):
+    def __init__(self):
         self.camera = cv2.VideoCapture(0)
         self.stopped = False
-        self.queue = Queue(maxsize=queueSize)
-        self.current = self.camera.read()
+        self.frame = self.camera.read()
 
     def start(self):
         thread = Thread(target=self.update, args=())
@@ -20,37 +17,24 @@ class CameraStream:
         thread.start()
         return self
 
-    def hasMore(self):
+    def running(self):
         return not self.stopped
-        # return self.queue.qsize() > 0
 
     def update(self):
         while True:
             if self.stopped:
                 return
 
-            _, self.current = self.camera.read()
-            # if self.queue.qsize() >= 2:
-            #     return
-            #
-            # # if not self.queue.full():
-            # grabbed, frame0 = self.camera.read()
-            # if not grabbed:
-            #     self.stop()
-            #     return
-            #
-            # self.queue.put(frame0)
+            _, self.frame = self.camera.read()
 
     def read(self):
-        return self.current
-        # return self.queue.get()
+        return self.frame
 
     def stop(self):
         self.stopped = True
         self.camera.release()
 
 
-# cap = cv2.VideoCapture(0)
 camera = CameraStream()
 camera.start()
 time.sleep(1)
@@ -64,7 +48,7 @@ while camera.hasMore():
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame and fps
-    cv2.putText(frame, "Slow", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+    cv2.putText(frame, fps.fps(), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
     # show frame
     cv2.imshow('frame', gray)
